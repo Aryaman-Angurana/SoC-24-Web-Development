ff133e819f180e29e9e539dbc9197843377b5d5d
Aryaman-Angurana <aryamanangurana26@gmail.com>
2024-06-06 12:30:53 +0530

    express exercise 1

---
+++ Week2/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+// app.use(logger)
+
+// app.get("/", logger, (req, res) => {
+//     res.render('index', {text: 'World'});
+// })
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const userRouter = require("./routes/users")
+
+app.use("/users", userRouter);
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
+
+app.listen(3000)
